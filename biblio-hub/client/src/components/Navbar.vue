b95724a302161395a0ab3038357d0d01d2c5f06d
knigawkl <lukasz.knigawka@outlook.com>
2019-11-23 01:15:50 +0100

    better-looking navbar

--- biblio-hub/client/src/components/Navbar.vue
+++ biblio-hub/client/src/components/Navbar.vue
@@ -1,22 +1,26 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-      <div class="pull-left">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+  <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
         <img src="../assets/logo.png" width="50%" alt="logo">
-      </div>
-      <div class="pull-right">
-        <ul class="nav">
-          <li style="float:right" v-if="!loggedIn">
-            <router-link :to="{ name: 'Login' }">Sign in</router-link>
-          </li>
-          <li style="float:right" v-if="!loggedIn">
-            <router-link :to="{ name: 'Register' }">Sign up</router-link>
-          </li>
-          <li style="float:right" v-if="loggedIn">
-            <router-link :to="{ name: 'Logout' }">Log out</router-link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+      </li>
+    </ul>
+  </div>
+  <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+    <ul class="navbar-nav ml-auto">
+      <li v-if="!loggedIn">
+        <router-link :to="{ name: 'Login' }">Sign in</router-link>
+      </li>
+      <li v-if="!loggedIn">
+        <router-link :to="{ name: 'Register' }">Sign up</router-link>
+      </li>
+      <li v-if="loggedIn">
+        <router-link :to="{ name: 'Logout' }">Log out</router-link>
+      </li>
+    </ul>
+  </div>
+</nav>
 </template>
 
 <script>
