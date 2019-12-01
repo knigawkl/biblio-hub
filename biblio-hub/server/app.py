9a826c6b5ee81b048269e3f9708e59c9a051f12d
knigawkl <lukasz.knigawka@outlook.com>
2019-12-02 00:01:04 +0100

    refactor

--- biblio-hub/server/app.py
+++ biblio-hub/server/app.py
@@ -38,7 +38,7 @@ def send_static(path):
 
 
 SWAGGER_URL = '/swagger'
-API_URL = '/static/swagg.yaml'
+API_URL = '/static/swag.yaml'
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
